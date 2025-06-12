aa17ed4e0f97f995517775bee26dd1c5025adeb8
Periapsises <94928308+Periapsises@users.noreply.github.com>
2025-06-12 04:51:29 +0000

    [Automatic] Update documentation

--- library/types/Markup.lua
+++ library/types/Markup.lua
@@ -3,12 +3,12 @@
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- The Markup type is used to easily format and draw text. Use render.parseMarkup(str, maxwidth) to create one.
 ---@class Markup
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L441).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L440).
 local Markup = {}
 
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Draw the markup object.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1930).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1937).
 ---@param x number # number The x offset
 ---@param y number # number The x offset
 ---@param xAlign number? # number The x TEXT_ALIGN. Default TEXT_ALIGN.LEFT
@@ -19,19 +19,19 @@ function Markup:draw(x, y, xAlign, yAlign, alpha, contentAlign) end
 
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Get the object height.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1948).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1955).
 ---@return number # The height of the object
 function Markup:getHeight() end
 
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Get the object size.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1954).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1961).
 ---@return number # The width of the object
 ---@return number # The height of the object
 function Markup:getSize() end
 
 --- ![](https://github.com/Periapsises/Starfall-LLS/blob/generator/resources/client.png?raw=true)
 --- Get the object width.
---- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1942).
+--- View [source](https://github.com/thegrb93/StarfallEx/blob/master/lua/starfall/libs_cl/render.lua#L1949).
 ---@return number # The width of the object
 function Markup:getWidth() end
